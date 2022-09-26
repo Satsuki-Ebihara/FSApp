48dbeef202bb3a29b8ecd20efaf288b519b638e0
Satsuki EBIHARA <m18026@g.metro-cit.ac.jp>
2022-09-26 10:42:02 +0900

    Initial Commit

---
+++ FSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FSApp
+//
+//  Created by 海老原颯希 on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
